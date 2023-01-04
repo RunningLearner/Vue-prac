e4e0bc685a4c3764274bf748ef5c2e11dbf95316
RunningLearner <tkacnsdms@naver.com>
2023-01-04 11:31:13 +0900

    feat: todo 틀 만들기

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,56 @@
 <template>
-  <div class="name">hi{{ name }}</div>
+  <div class="container">
+    <h2>To-Do List</h2>
+    <form @submit.prevent="onSubmit" class="d-flex">
+      <div class="flex-grow-1 mr-2">
+        <input
+          class="form-control"
+          type="text"
+          v-model="todo"
+          placeholder="Type new to-do"
+        />
+      </div>
+      <div>
+        <button class="btn btn-primary" type="submit">Add</button>
+      </div>
+    </form>
+    {{ todos }}
+    <div class="card">
+      <div class="card-body p-2">
+        {{ todos[0].subject }}
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-body p-2">
+        {{ todos[1].subject }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
   setup() {
-    const name = " seunam!";
-    return { name };
+    const todo = ref("");
+    const todos = ref([
+      {
+        id: 1,
+        subject: "go home",
+      },
+      {
+        id: 2,
+        subject: "go school",
+      },
+    ]);
+
+    const onSubmit = () => {
+      todos.value.push({ id: Date.now(), subject: todo.value });
+    };
+
+    return { onSubmit, todo, todos };
   },
 };
 </script>
 
-<style>
-.name {
-  color: red;
-}
-</style>
+<style></style>
