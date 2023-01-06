d0ef910acd5e8c7c6bc8def162d518b4068a51c3
RunningLearner <tkacnsdms@naver.com>
2023-01-06 11:06:57 +0900

    refact: 구조분해할당으로 불필요한 코드 줄임, vue3 기준으로 emit 목록 명시

--- src/components/TodoSimpleForm.vue
+++ src/components/TodoSimpleForm.vue
@@ -21,7 +21,8 @@
 import { ref } from "vue";
 
 export default {
-  setup(props, context) {
+  emits: ["add-todo"],
+  setup(props, { emit }) {
     const todo = ref("");
     const hasError = ref(false);
 
@@ -29,7 +30,7 @@ export default {
       if (todo.value === "") {
         hasError.value = true;
       } else {
-        context.emit("add-todo", {
+        emit("add-todo", {
           id: Date.now(),
           subject: todo.value,
           completed: false,
