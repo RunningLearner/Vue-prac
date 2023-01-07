c68e35df8b7cc3f769d5bab40083ab936b6ec3fa
RunningLearner <tkacnsdms@naver.com>
2023-01-07 10:34:21 +0900

    feat: json-server & axios 추가

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="container">
     <h2>To-Do List</h2>
+    <input
+      class="form-control"
+      type="text"
+      v-model="searchText"
+      placeholder="Search..."
+    />
+    <hr />
     <TodoSimpleForm @add-todo="addTodo" />
 
-    <div v-if="!todos.length">추가된 Todo가 없습니다!</div>
+    <div v-if="!filteredTodos.length">추가된 Todo가 없습니다!</div>
     <TodoList
-      :todos="todos"
+      :todos="filteredTodos"
       @toggle-todo="toggleTodo"
       @delete-todo="deleteTodo"
     />
@@ -13,7 +20,7 @@
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import TodoSimpleForm from "./components/TodoSimpleForm.vue";
 import TodoList from "./components/TodoList.vue";
 
@@ -37,7 +44,25 @@ export default {
       todos.value[index].completed = !todos.value[index].completed;
     };
 
-    return { addTodo, todos, deleteTodo, toggleTodo };
+    const searchText = ref("");
+    const filteredTodos = computed(() => {
+      if (searchText.value) {
+        return todos.value.filter((todo) => {
+          return todo.subject.includes(searchText.value);
+        });
+      }
+
+      return todos.value;
+    });
+
+    return {
+      searchText,
+      addTodo,
+      todos,
+      deleteTodo,
+      toggleTodo,
+      filteredTodos,
+    };
   },
 };
 </script>
