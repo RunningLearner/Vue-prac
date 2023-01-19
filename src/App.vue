d6eb0a8cef26f7a3538915c55abca327718d054c
RunningLearner <tkacnsdms@naver.com>
2023-01-19 20:09:40 +0900

    feat: navbar 추가

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,15 @@
 <template>
+  <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/">Seunam</a>
+
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/todos">Todos</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
   <router-view />
 </template>
 
