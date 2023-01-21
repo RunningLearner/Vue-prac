81f940a2276282619624b53fd34a8d6d55af22c7
RunningLearner <tkacnsdms@naver.com>
2023-01-21 10:01:57 +0900

    feat: 상태와 캔슬 추가

--- src/pages/todos/_id.vue
+++ src/pages/todos/_id.vue
@@ -2,22 +2,46 @@
   <h1>To-Do Page</h1>
   <div v-if="loading">Loading...</div>
   <form v-else>
-    <div class="form-group">
-      <label>Todo Subject</label>
-      <input v-model="todo.subject" type="text" class="form-control" />
+    <div class="row">
+      <div class="col-6">
+        <div class="form-group">
+          <label>Subject</label>
+          <input v-model="todo.subject" type="text" class="form-control" />
+        </div>
+      </div>
+      <div class="col-6">
+        <div class="form-group">
+          <label>Status</label>
+          <div>
+            <button
+              type="button"
+              class="btn"
+              :class="todo.completed ? 'btn-success' : 'btn-danger'"
+              @click="toggleTodoStatus"
+            >
+              {{ todo.completed ? "Completed" : "Incomplete" }}
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-    <button class="btn btn-primary">Save</button>
+
+    <button type="submit" class="btn btn-primary">Save</button>
+    <button class="btn btn-outline-dark ml-2" @click="moveToTodoListPage">
+      Cancel
+    </button>
   </form>
 </template>
 
 <script>
 import axios from "axios";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import { ref } from "vue";
 
 export default {
   setup() {
     const route = useRoute();
+    const router = useRouter();
     const todo = ref(null);
     const loading = ref(true);
 
@@ -29,11 +53,21 @@ export default {
       loading.value = false;
     };
 
+    const toggleTodoStatus = () => {
+      todo.value.completed = !todo.value.completed;
+    };
+
     getTodo();
-    console.log(route.params.id);
+
+    const moveToTodoListPage = () => {
+      router.push({ name: "Todos" });
+    };
+
     return {
       todo,
       loading,
+      toggleTodoStatus,
+      moveToTodoListPage,
     };
   },
 };
