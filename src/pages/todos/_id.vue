2d7dd149ea9b685c448c9edec50fa53b3c73aead
RunningLearner <tkacnsdms@naver.com>
2023-01-21 10:18:50 +0900

    feat: 데이터 수정후 저장기능 추가

--- src/pages/todos/_id.vue
+++ src/pages/todos/_id.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>To-Do Page</h1>
   <div v-if="loading">Loading...</div>
-  <form v-else>
+  <form v-else @submit.prevent="onSave">
     <div class="row">
       <div class="col-6">
         <div class="form-group">
@@ -44,11 +44,10 @@ export default {
     const router = useRouter();
     const todo = ref(null);
     const loading = ref(true);
+    const todoId = route.params.id;
 
     const getTodo = async () => {
-      const res = await axios.get(
-        "http://localhost:3000/todos/" + route.params.id
-      );
+      const res = await axios.get(`http://localhost:3000/todos/${todoId}`);
       todo.value = res.data;
       loading.value = false;
     };
@@ -59,6 +58,13 @@ export default {
 
     getTodo();
 
+    const onSave = async () => {
+      await axios.put(`http://localhost:3000/todos/${todoId}`, {
+        subject: todo.value.subject,
+        completed: todo.value.completed,
+      });
+    };
+
     const moveToTodoListPage = () => {
       router.push({ name: "Todos" });
     };
@@ -68,6 +74,7 @@ export default {
       loading,
       toggleTodoStatus,
       moveToTodoListPage,
+      onSave,
     };
   },
 };
