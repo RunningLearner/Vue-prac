58ea796dfd3491b1c9f4a751ebda5200ffb962c2
RunningLearner <tkacnsdms@naver.com>
2023-01-22 09:41:04 +0900

    feat: 기본 modal 컴포넌트화

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -3,19 +3,18 @@
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title fs-5">Delete Todo</h5>
+          <h5 class="modal-title fs-5">
+            <slot name="title"> </slot>
+          </h5>
           <button type="button" class="btn-close">
             <span @click="onClose">&times;</span>
           </button>
         </div>
-        <div class="modal-body">Are you sure to delete?</div>
+        <div class="modal-body">
+          <slot name="body"> </slot>
+        </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" @click="onClose">
-            Close
-          </button>
-          <button type="button" class="btn btn-danger" @click="onDelete">
-            Delete
-          </button>
+          <slot name="footer"> </slot>
         </div>
       </div>
     </div>
@@ -29,11 +28,7 @@ export default {
       emit("close");
     };
 
-    const onDelete = () => {
-      emit("delete");
-    };
-
-    return { onClose, onDelete };
+    return { onClose };
   },
 };
 </script>
